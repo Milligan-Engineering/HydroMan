ba939174c2cac9cfdbc4858742d77af6a19c7428
john rutherford <jmrutherford@my.milligan.edu>
2019-02-22 10:09:00 -0500

    Final Commit

--- HydroMan/HydroMan.cpp
+++ HydroMan/HydroMan.cpp
@@ -42,206 +42,166 @@ int main()
 	double workoutDate;
 	double timeDifference;
 
-
-	cout << "S - Setup Workouts. \nW - Track Water. \nP - Change Settings.";
-	cin >> waterOrWorkout;
-
-	switch (waterOrWorkout)
+	do
 	{
-	case 'S':
-		// telling current time
-		time_t(currentTime);
-		currentTime = time(NULL);
-		cout << currentTime << " seconds has passed since 00:00:00 GMT, Jan 1, 1970\n";
-		
-		//date format converter
-
-		cout << "enter workout month, day and year.\n";
-		cin >> monthVal;
-
-			while ((monthVal < 1) || (monthVal > 12))
-			{
-			cout << "month input must be from 1 to 12.\n";
-			cin >> monthVal;
-			}
-
-		cin >> dayVal;
-
-			while ((dayVal < 1) || (dayVal > 31))
-			{
-			cout << "day input must be from 1 to 31.\n";
-			cin >> dayVal;
-			}
-
-		cin >> yearVal;
-
-			while ((yearVal < 2019) || (monthVal > 2099))
-			{
-			cout << "year input must be from 2019 to 2099.\n";
-			cin >> yearVal;
-			}
-
-		cout << "you have selected " << monthVal << ", " << dayVal << ", " << yearVal << "\n";
-		workoutDate = timeConverter(monthVal, dayVal, yearVal);
-		
-
-		cout << "you selected a workout date for.\n" << workoutDate;
-
-		timeDifference = workoutDate - currentTime;
-		cout << "time until workout is " << timeDifference << "\n";
-
-		// Validation
-		cout << "What would you like to do next? \n";
-		cout << "S - Setup Workouts. \n";
-		cout << "W - Track Water. \n";
-		cout << "P - Change Settings. \n";
+		cout << "S - Setup Workouts. \nW - Track Water. \nP - Change Settings.\nE - Exit Program.";
 		cin >> waterOrWorkout;
 
-		// Validation
-		while ((waterOrWorkout != 'W') && (waterOrWorkout != 'S') && (waterOrWorkout != 'P'))
+		switch (waterOrWorkout)
 		{
-			cout << "invalid input please enter W, S or P.\n";
-			cin >> waterOrWorkout;
-		}
+			case 'S':
+			{
+				// telling current time
+				time_t(currentTime);
+				currentTime = time(NULL);
+				cout << currentTime << " seconds has passed since 00:00:00 GMT, Jan 1, 1970\n";
 
-	case 'W':
-		cout << "We are now ready to track water for the day!\n";
+				//date format converter
 
-		// do while loop to track water
+				cout << "enter workout month, day and year.\n";
+				cin >> monthVal;
 
-		totalOzOfWater = 0;
-
-		do
-		{
-			cout << "Enter oz of water\n";
-			cin >> ozOfWater;
-
-				while ((ozOfWater < 0) || (ozOfWater > 33))
+				while ((monthVal < 1) || (monthVal > 12))
 				{
-				cout << "Please enter a valid water input from 1 to 32 oz.\n";
-				cin >> ozOfWater;
+					cout << "month input must be from 1 to 12.\n";
+					cin >> monthVal;
 				}
-	
-			totalOzOfWater = ozOfWater + totalOzOfWater;
-			cout << "You have currently drank " << totalOzOfWater << " oz. of water\n";
-			cout << "do you want to continue? Y/N?";
-			cin >> moreInputs;
-		} while (moreInputs == 'Y');
-		
 
-		cout << "total water for the day is " << totalOzOfWater;
+				cin >> dayVal;
 
-		// redirection menu
-		cout << "What would you like to do next? \n";
-		cout << "S - Setup Workouts. \n";
-		cout << "W - Track Water. \n";
-		cout << "P - Change Settings. \n";
-		cin >> waterOrWorkout;
+				while ((dayVal < 1) || (dayVal > 31))
+				{
+					cout << "day input must be from 1 to 31.\n";
+					cin >> dayVal;
+				}
 
-		// Validation
-		while ((waterOrWorkout != 'W') && (waterOrWorkout != 'S') && (waterOrWorkout != 'P'))
-		{
-			cout << "invalid input please enter W, S or P.\n";
-			cin >> waterOrWorkout;
+				cin >> yearVal;
+
+				while ((yearVal < 2019) || (monthVal > 2099))
+				{
+					cout << "year input must be from 2019 to 2099.\n";
+					cin >> yearVal;
+				}
+
+				cout << "you have selected " << monthVal << ", " << dayVal << ", " << yearVal << "\n";
+				workoutDate = timeConverter(monthVal, dayVal, yearVal);
+
+
+				cout << "you selected a workout date for.\n" << workoutDate;
+
+				timeDifference = workoutDate - currentTime;
+				cout << "time until workout is " << timeDifference << "\n";
+				break;
+			}
+			case 'W':
+			{
+				cout << "We are now ready to track water for the day!\n";
+
+				// do while loop to track water
+
+				totalOzOfWater = 0;
+
+				do
+				{
+					cout << "Enter oz of water\n";
+					cin >> ozOfWater;
+
+					while ((ozOfWater < 0) || (ozOfWater > 33))
+					{
+						cout << "Please enter a valid water input from 1 to 32 oz.\n";
+						cin >> ozOfWater;
+					}
+
+					totalOzOfWater = ozOfWater + totalOzOfWater;
+					cout << "You have currently drank " << totalOzOfWater << " oz. of water\n";
+					cout << "do you want to continue? Y/N?";
+					cin >> moreInputs;
+				} while (moreInputs == 'Y');
+
+
+				cout << "total water for the day is " << totalOzOfWater << "\n";
+				break;
+			}
+			case 'P':
+			{
+				cout << "Lets set up your user profile!\n";
+
+
+				cout << "age?\n";
+				cin >> age;
+
+				//validating age
+				while ((age < 5) || (age > 100))
+				{
+					cout << "invalid input please enter an age 5 to 100.\n";
+					cin >> age;
+				}
+
+				cout << "weight in lbs?\n";
+				cin >> weight;
+
+				//validating weight
+				while ((weight < 80) || (weight > 500))
+				{
+					cout << "invalid input please enter a weight 80lbs to 500lbs.\n";
+					cin >> weight;
+				}
+
+				cout << "height in Ft and In?\n";
+				cin >> heightFt >> heightIn;
+
+				//validating height
+				while ((heightFt < 3) || (heightFt > 7))
+				{
+					cout << "invalid input please enter a height from 3FT and 7FT.\n";
+					cin >> heightFt;
+				}
+
+				while ((heightIn < 1) || (heightIn > 11))
+				{
+					cout << "invalid input please enter a height from 0IN to 12IN.\n";
+					cin >> heightIn;
+
+				}
+
+				cout << "Your current proflie settings are.\n" << "\n"
+					<< age << "Years old \n"
+					<< weight << "lbs \n"
+					<< heightFt << "Ft " << heightIn << "In \n";
+				todayWaterNeeded = baseWaterNeeded(heightIn, heightFt, age, weight, gender);
+				cout << "based on your profile you need to drink " << todayWaterNeeded << " oz of water per day\n";
+				break;
+			}
 		}
-	
-
-	case 'P':
-		cout << "Lets set up your user profile!\n";
-
-
-		cout << "age?\n";
-		cin >> age;
-
-		//validating age
-		while ((age < 5) || (age > 100))
-		{
-			cout << "invalid input please enter an age 5 to 100.\n";
-			cin >> age;
-		}
-
-		cout << "weight in lbs?\n";
-		cin >> weight;
-
-		//validating weight
-		while ((weight < 80) || (weight > 500))
-		{
-			cout << "invalid input please enter a weight 80lbs to 500lbs.\n";
-			cin >> weight;
-		}
-
-		cout << "height in Ft and In?\n";
-		cin >> heightFt >> heightIn;
-
-		//validating height
-		while ((heightFt < 3) || (heightFt > 7))
-		{
-			cout << "invalid input please enter a height from 3FT and 7FT.\n";
-			cin >> heightFt;
-		}
-
-		while ((heightIn < 1) || (heightIn > 11))
-		{
-			cout << "invalid input please enter a height from 0IN to 12IN.\n";
-			cin >> heightIn;
-
-		}
-
-		cout << "Your current proflie settings are.\n" << "\n"
-			<< age << "Years old \n"
-			<< weight << "lbs \n"
-			<< heightFt << "Ft " << heightIn << "In \n";
-		todayWaterNeeded = baseWaterNeeded(heightIn, heightFt, age, weight, gender);
-		cout << "based on your profile you need to drink " << todayWaterNeeded << " oz of water per day\n";
-
-		// Redirection menu
-		cout << "What would you like to do next? \n";
-		cout << "S - Setup Workouts. \n";
-		cout << "W - Track Water. \n";
-		cout << "P - Change Settings. \n";
-		cin >> waterOrWorkout;
-
-		// Validation
-		while ((waterOrWorkout != 'W') && (waterOrWorkout != 'S') && (waterOrWorkout != 'P'))
-		{
-			cout << "invalid input please enter W, S or P.\n";
-			cin >> waterOrWorkout;
-		}
-
-	}
+	} while (waterOrWorkout != 'E');
 
 
 
-
-	while ((waterOrWorkout != 'W') && (waterOrWorkout != 'S') && (waterOrWorkout != 'P'))
-	{
-		cout << "invalid input please enter W, S or P.\n";
-		cin >> waterOrWorkout;
-	}
 }
-	double baseWaterNeeded(int heightInPar, int heightFtPar, int agePar, int weightPar, char genderPar)
-	{
-		const double weightConst = .5;
-		double waterNeeded;
-		waterNeeded = weightPar * .5;
-		return(waterNeeded);
-	}
-
-	double timeConverter(int monthVal, int dayVal, int yearVal)
-	{
-
-		double timeFormat;
-		int daysOfMonth[12] = { 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31 };
-		timeFormat = (yearVal - 1970) * 365;
-		timeFormat += (yearVal - 1970) / 4;
-		for (int i = 1; i < monthVal; i++)
+	
+		double baseWaterNeeded(int heightInPar, int heightFtPar, int agePar, int weightPar, char genderPar)
 		{
-			timeFormat += daysOfMonth[i];
+			const double weightConst = .5;
+			double waterNeeded;
+			waterNeeded = weightPar * .5;
+			return(waterNeeded);
 		}
-		timeFormat += dayVal;
-		timeFormat = timeFormat * 24 * 60 * 60;
-		cout << timeFormat;
-		return(timeFormat);
-	}
+
+		double timeConverter(int monthVal, int dayVal, int yearVal)
+		{
+
+			double timeFormat;
+			int daysOfMonth[12] = { 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31 };
+			timeFormat = (yearVal - 1970) * 365;
+			timeFormat += (yearVal - 1970) / 4;
+			for (int i = 0; i < monthVal - 1; i++)
+			{
+				timeFormat += daysOfMonth[i];
+			}
+			timeFormat += dayVal - 1;
+			timeFormat = timeFormat * 24 * 60 * 60;
+			cout << timeFormat;
+			return(timeFormat);
+		}
 
 	
